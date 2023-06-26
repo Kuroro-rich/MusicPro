baf1f088e9f8752800f97a7b8492a05e198e0960
kuroro-rich <rich.emartinez@hotmail.com>
2023-06-25 20:51:17 -0400

    cambios de todo

--- app/app.py
+++ app/app.py
@@ -1,15 +1,33 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app=Flask(__name__)
 
 @app.route('/')
-def index():
-   # return "Hola mundo- asi"
-   data= {
-       'titulo': 'Music Pro',
-       'Bienvenidos': 'Saludos cordialmente'
-   }
-   return render_template('index.html', data=data)
+def index ():
+    return render_template('indexCli.html')
+
+@app.route('/pedidos')
+def pedidos ():
+    return render_template('pedidosCli.html')
+
+@app.route('/carro')
+def carro ():
+    return render_template('carroCli.html')
+
+@app.route('/productos')
+def productos ():
+    return render_template('prodCli.html')
+
+@app.route('/login')
+def login ():
+    return render_template('loginCli.html')
+
+@app.route('/register')
+def register ():
+    return render_template('registerCli.html')
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True,port=5000)
+    
